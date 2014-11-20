7fcc2047ea2a29dc8c64a33e4a7e3dcb3baaadc2
marqh <markh@scitools.org.uk>
2014-11-20 10:43:04 +0000

    andon

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+import numpy as np
+
+setup(
+  name = 'udunits2python',
+  version = '0.1',
+  description = 'Python package for accessing udunits2 from unidata',
+  package_dir = {'': 'lib'},
+  packages = ['udunits'],
+  ext_modules =  Extension('udunits.__init__', ['lib/udunits/__init__.pyx'],
+                           include_dirs=[get_config_var('INCLUDEDIR'), np.get_include()],
+                  libraries=['netcdf'],
+                  library_dirs=[get_config_var('LIBDIR')],
+                  **extra_extension_args
+                  ),
+)
+
